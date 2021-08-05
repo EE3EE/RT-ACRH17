8e470f4f11ada2e6cb59e4eb3d0daaeb9b6a2ece
ATRI <68682142+EE3EE@users.noreply.github.com>
2021-08-05 19:29:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+# Ôö¼Óhttps://github.com/kenzok8/openwrt-packagesµ½¸üÐÂÔ´
+# src-git kenzo https://github.com/kenzok8/openwrt-packages
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# fw876/helloworld
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#»ñÈ¡Lienol-xiaorouji-passwall
+# git clone https://github.com/xiaorouji/openwrt-package/lienol/ package/diy-packages/lienol
+# git clone https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall package/luci-app-passwall
+# git clone https://github.com/kenzok8/openwrt-packages.git package/diy-packages
+# git clone https://github.com/kenzok8/small.git package/small
+
+
+
+# Ôö¼Óssr
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
