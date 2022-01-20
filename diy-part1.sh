1e21934b909fef0e94c024a4ae52e1b665189cc4
ATRI <68682142+EE3EE@users.noreply.github.com>
2022-01-20 16:12:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,4 +25,4 @@
 # Ôö¼Óssr
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
-echo 'src-git packages https://github.com/coolsnowwolf/lede.git
+# echo 'src-git packages https://github.com/coolsnowwolf/lede.git
